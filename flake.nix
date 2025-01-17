20a371b7c8f34ca898c624d9f6b79cb633c16552
Sreehari Sreedev <me@ssree.dev>
2025-01-17 03:21:43 -0700

    mbp

--- flake.nix
+++ flake.nix
@@ -14,21 +14,26 @@
       url = "gitlab:doronbehar/nix-matlab";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ancient-nixpkgs.url = "github:nixos/nixpkgs/09704d31f17bb6eb2de763ac31ec3624b811f57c";
   };
 
   outputs =
-    {
-      nixpkgs,
-      home-manager,
-      lanzaboote,
-      nix-matlab,
-      ...
-    }@inputs:
+    { nixpkgs, ... }@inputs:
     let
       inherit (nixpkgs) lib;
       specialArgs = { inherit inputs; };
       forAllSystems =
         body: lib.genAttrs lib.systems.flakeExposed (system: body nixpkgs.legacyPackages.${system});
+      overlays = [
+        (final: prev: {
+          zigpkgs = inputs.zig-overlay.packages.${prev.system};
+          ancient = inputs.ancient-nixpkgs.legacyPackages.${prev.system};
+        })
+      ];
     in
     {
       nixosConfigurations = {
@@ -37,23 +42,34 @@
           modules = [
             ./common
             ./t480s
-            home-manager.nixosModules.home-manager
+            inputs.home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.sreehari = import ./home;
             }
           ];
+
         };
         riptide = nixpkgs.lib.nixosSystem {
           inherit specialArgs;
           modules = [
-            lanzaboote.nixosModules.lanzaboote
+            inputs.lanzaboote.nixosModules.lanzaboote
             ./common
             ./riptide
             ./riptide/matlab.nix
           ];
         };
+      };
+      # macOS home-manager
+      homeConfigurations."user" = inputs.home-manager.lib.homeManagerConfiguration {
+        extraSpecialArgs = specialArgs;
+        pkgs = import nixpkgs {
+          # is there somewhere else to define the system??
+          system = "aarch64-darwin";
+          inherit overlays;
+        };
+        modules = [ ./mbp ];
       };
       formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
     };
---
+++ mbp/default.nix
@@ -0,0 +1,84 @@
+{
+  config,
+  pkgs,
+  nixpkgs,
+  ...
+}:
+{
+  home.username = "user";
+  home.homeDirectory = "/Users/user";
+
+  home.packages = with pkgs; [
+    # development
+    #zig
+    tree
+    flashrom
+    zigpkgs.master-2025-01-03
+    bun
+    acpica-tools
+    ancient.neovim
+    nodejs
+    nil
+    lua-language-server
+    macfuse-stubs
+    typst
+    typst-lsp
+
+    # fonts
+    nerd-fonts.fira-code
+
+    # tools
+    tt
+    neofetch
+    fastfetch
+    cmatrix
+    ripgrep
+    mp4v2
+    kitty
+    gimp
+    graphviz
+    (python3.withPackages (
+      ppkgs: with ppkgs; [
+        numpy
+        pwntools
+        pandas
+        pydot
+        ipython
+        #z3
+        torch-bin
+        torchvision-bin
+        matplotlib
+        flask
+        flask-cors
+        neo4j
+        bcrypt
+      ]
+    ))
+    p7zip
+    age
+    verilog
+    gtkwave
+    mtr
+    gprolog
+    b3sum
+    android-tools
+    exiftool
+    meson
+    minisign
+    idevicerestore
+    libimobiledevice
+    bat
+    qemu
+  ];
+
+  home.file = { };
+  home.sessionVariables = { };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowBroken = true;
+
+  # DO NOT CHANGE
+  home.stateVersion = "23.11";
+}
